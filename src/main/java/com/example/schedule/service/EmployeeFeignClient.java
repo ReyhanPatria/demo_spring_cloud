b7880fe4442e161e3a9c651032cc874a978e5a70
ReyhanPatria <reyhan2patria@gmail.com>
2021-08-03 15:51:26 +0700

    Changed EmployeeFeignClient url

--- src/main/java/com/example/schedule/service/EmployeeFeignClient.java
+++ src/main/java/com/example/schedule/service/EmployeeFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "employeeFeignClient", url = "http://localhost:8000") 
+@FeignClient(name = "employeeFeignClient", url = "https://demo-project-employee.herokuapp.com/") 
 public interface EmployeeFeignClient {
     @GetMapping(value = "employee/{id}")
     public EmployeePojo getEmployeeById(@PathVariable Integer id);
